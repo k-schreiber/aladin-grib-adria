1082d4dbc48a21be30386e1b4ae963e846507a21
k-schreiber <konrad-schreiber@gmx.de>
2025-10-10 23:51:44 +0200

    Use public Dockerfile

--- start.sh
+++ start.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-echo "0 * * * * /app/process.sh >> /var/log/process.log 2>&1" > /etc/cron.d/gribproc
-chmod 0644 /etc/cron.d/gribproc
-crontab /etc/cron.d/gribproc
-
+# Run process.sh once in background and schedule hourly updates
 /app/process.sh || true &
 (crond -f &) &
 
+# Start Flask server
 exec python3 /app/server.py
